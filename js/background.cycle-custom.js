880bcc162818c6815dac92db1a32e47f9da9ea0b
kyle <aw133e@gmail.com>
2020-07-09 13:27:08 +0800

    add color choose page for dev use

--- js/background.cycle-custom.js
+++ js/background.cycle-custom.js
@@ -1,1 +1,35 @@
-// JavaScript Documentvar fadeShow = $(".background").fadeShow({	correctRatio: true,	shuffle: true,	speed: 5000,	images: [		'../images/1.jpg',		'../images/2.jpg',		'../images/3.jpg'	]});
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 5000,
+
+
+
+	images: [
+		'../images/1.jpg',
+		'../images/2.jpg',
+		'../images/3.jpg',
+		'../images/4.jpg',
+		'../images/5.jpg',
+		'../images/6.jpg',
+		'../images/7.jpg'
+	]
+
+
+
+
+
+
+
+});
